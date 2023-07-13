c3b47e67e760aea5c841985ab4d44fb36cff1dae
Derrick Hammer <derrick@derrickhammer.com>
2023-07-13 02:53:23 -0400

    fix: add optimisticUpdateCallback to client factory

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,16 +2,20 @@ import Client from "./client.js";
 import Prover, { ProverRequestCallback } from "../prover.js";
 import VerifyingProvider from "./verifyingProvider.js";
 import Store from "#store.js";
+import { BaseClientOptions } from "#baseClient.js";
+import { OptimisticUpdateCallback } from "#types.js";
 
 function createDefaultClient(
   proverHandler: ProverRequestCallback,
   rpcHandler: Function,
+  optimisticUpdateHandler: OptimisticUpdateCallback,
 ): Client {
   return new Client({
     prover: new Prover(proverHandler),
     store: new Store(60 * 60),
     provider: VerifyingProvider,
     rpcHandler,
+    optimisticUpdateCallback: optimisticUpdateHandler,
   });
 }
 
